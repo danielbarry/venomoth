089a4dc41b3fcc940041331ab5b26238f3034f4a
Dan <danbarry16@googlemail.com>
2016-07-20 10:29:05 +0100

    Header file for Main class

---
+++ src/main.hh
@@ -0,0 +1,22 @@
+/**
+ * main.hh
+ *
+ * This class is responsible for taking the input from the user and setting up
+ * the main program.
+ **/
+class Main{
+  public :
+    /**
+     * Main()
+     *
+     * The constructor for the Main class.
+     **/
+    Main();
+
+    /**
+     * ~Main()
+     *
+     * The destructor for the main class.
+     **/
+    ~Main();
+};
