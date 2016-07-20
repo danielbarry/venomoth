1a1db81de23968f21d28acf0075e7bd5ba3c70dd
Dan <danbarry16@googlemail.com>
2016-07-20 10:29:30 +0100

    Implementation of main class

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+#include "main.hh"
+
+/**
+ * main()
+ *
+ * The main entry point of the program.
+ **/
+int main(int argc, char** argv){
+  /* TODO: Process user input. */
+}
+
+Main::Main(){
+  /* TODO: Write this section. */
+}
+
+Main::~Main(){
+  /* TODO: Write this section. */
+}
