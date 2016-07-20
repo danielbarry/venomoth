c1be979b0c00f7aff5b55df93cb8209999f3b651
Dan <danbarry16@googlemail.com>
2016-07-20 14:14:41 +0100

    Empty View class

---
+++ src/view.hh
@@ -0,0 +1,9 @@
+/**
+ * view.hh
+ *
+ * Defines an interface that can be used and translated into the specific mode
+ * of output to the user. Example uses may be the terminal, GUI, etc.
+ **/
+class View{
+  /* TODO: Write the class. */
+};
